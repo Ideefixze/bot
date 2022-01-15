fea4f0b614cdeb83d2bbbff57905da000734a42d
Maciej Jur <mj@kamoshi.org>
2022-01-15 12:57:57 +0100

    Use .env for configuration

--- bot.py
+++ bot.py
@@ -1,10 +1,12 @@
-import discord
-import secret
 from discord.ext import commands
+from dotenv import dotenv_values
 
 from cogs.hello import Greetings
+
 
 bot = commands.Bot(command_prefix="=")
 bot.add_cog(Greetings(bot))
 
-bot.run(secret.TOKEN)
+
+config = dotenv_values(".env")
+bot.run(config["TOKEN"])
